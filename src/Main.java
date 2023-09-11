b3c3503f7c3d86db76f5c8b3baba2f34a49a8611
MuLangballe <mala0007@stud.kea.dk>
2023-09-11 15:42:48 +0200

    klasser oprettet

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Superhelte database");
+        System.out.println("Tilføj superhelt: indtast superheltens navn: ");
+        Scanner keyboard = new Scanner(System.in);
+        String superHeroName = keyboard.next();
 
+        System.out.println("indtast superheltens RIGTIGE navn: ");
+        String superHeroRealName = keyboard.next();
     }
 }
