5fbef211a586796ff11890e7851c5da242c861c3
MuLangballe <mala0007@stud.kea.dk>
2023-09-11 19:41:02 +0200

    Attributter, konstruktør og getter/setter metoder i superhero klassen

--- src/Superhero.java
+++ src/Superhero.java
@@ -1,2 +1,66 @@
 public class Superhero {
+    private String name;
+    private String realName;
+    private String superPower;
+    private int yearCreated;
+    private boolean isHuman;
+    private int strength;
+
+    public Superhero(String name, String realName, String superPower, int yearCreated, boolean isHuman, int strength) {
+        this.name = name;
+        this.realName = realName;
+        this.superPower = superPower;
+        this.yearCreated = yearCreated;
+        this.isHuman = isHuman;
+        this.strength = strength;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRealName() {
+        return realName;
+    }
+
+    public void setRealName(String realName) {
+        this.realName = realName;
+    }
+
+    public String getSuperPower() {
+        return superPower;
+    }
+
+    public void setSuperPower(String superPower) {
+        this.superPower = superPower;
+    }
+
+    public int getYearCreated() {
+        return yearCreated;
+    }
+
+    public void setYearCreated(int yearCreated) {
+        this.yearCreated = yearCreated;
+    }
+
+    public boolean isHuman() {
+        return isHuman;
+    }
+
+    public void setHuman(boolean human) {
+        isHuman = human;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public void setStrength(int strength) {
+        this.strength = strength;
+    }
 }
