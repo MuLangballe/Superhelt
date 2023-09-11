6de5d5e90d0bed588bd57075a501d88472944398
MuLangballe <mala0007@stud.kea.dk>
2023-09-11 21:19:16 +0200

    løst userstory 1 & 2 (delopgave 1) create superhero metode og array, scannere til brugerinput menu og loop

--- src/Database.java
+++ src/Database.java
@@ -1,2 +1,13 @@
 public class Database {
+    private Superhero[] superheroes;
+    int count = 0;
+
+    public Database(){
+        this.superheroes = new Superhero[1];
+    }
+
+    public void createSuperhero(String name, String realName, String superPower, int yearCreated, boolean isHuman, int strength){
+        superheroes[count++] = new Superhero(name, realName, superPower, yearCreated, isHuman, strength);
+
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,41 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Superhelte database");
-        System.out.println("Tilføj superhelt: indtast superheltens navn: ");
-        Scanner keyboard = new Scanner(System.in);
-        String superHeroName = keyboard.next();
+        int menuChoice;
+        do {
+            System.out.println("Velkommen til SUPERHERO UNIVERSET. \n 1. Opret superhelt \n 9. Afslut");
+            Scanner keyboard = new Scanner(System.in);
+            menuChoice = keyboard.nextInt();
+            if (menuChoice == 1) {
+                System.out.println("Tilføj superhelt: indtast superheltens navn: ");
+                String superHeroName = keyboard.next();
 
-        System.out.println("indtast superheltens RIGTIGE navn: ");
-        String superHeroRealName = keyboard.next();
+                System.out.println("Indtast superheltens RIGTIGE navn: ");
+                String superHeroRealName = keyboard.next();
+
+                System.out.println("Indtast superheltens superkraft: ");
+                String superHeroSuperPower = keyboard.next();
+
+                System.out.println("Indtast året superhelten blev skabt: ");
+                int superHeroYearCreated = keyboard.nextInt();
+
+                System.out.println("Er superhelten et menneske? [j/n]");
+                char isHumanInput = keyboard.next().charAt(0);
+                boolean isSuperheroHuman = true;
+                switch (isHumanInput) {
+                    case 'j' -> {
+                    }
+                    case 'n' -> isSuperheroHuman = false;
+                }
+
+                System.out.println("Hvor stærk er superhelten(i tal)?");
+                int superHeroStrength = keyboard.nextInt();
+
+                Database superheroDatabase = new Database();
+
+                superheroDatabase.createSuperhero(superHeroName, superHeroRealName, superHeroSuperPower, superHeroYearCreated, isSuperheroHuman, superHeroStrength);
+            }
+        } while (menuChoice != 9);
     }
 }
+
