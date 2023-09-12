a99465f50baf3e5756cbc03ab9d18f7499136a41
MuLangballe <mala0007@stud.kea.dk>
2023-09-12 15:44:48 +0200

    ryddet op i Database class

--- src/Database.java
+++ src/Database.java
@@ -3,10 +3,6 @@ import java.util.ArrayList;
 public class Database {
     ArrayList<Superhero> superheroes = new ArrayList<>();
 
-    public Database(){
-
-    }
-
     public void createSuperhero(String name, String realName, String superPower, int yearCreated, boolean isHuman, int strength){
          superheroes.add(new Superhero(name, realName, superPower, yearCreated, isHuman, strength));
     }
