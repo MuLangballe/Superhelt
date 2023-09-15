e5180c9b0352cf178e0be731f4beb071cebad230
MuLangballe <mala0007@stud.kea.dk>
2023-09-15 09:09:01 +0200

    UserInterface class created, startProgram method created, user dialog moved til userInterface

---
+++ src/UserInterface.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+
+public class UserInterface {
+    public void startProgram() {
+        int menuChoice;
+        Database superheroDatabase = new Database();
+
+        do {
+            System.out.println("Velkommen til SUPERHERO UNIVERSET. \n 1. Opret superhelt \n 2. Se liste af superhelte \n 3. Søg efter superhelt \n 9. Afslut");
+            Scanner keyboard = new Scanner(System.in);
+            menuChoice = keyboard.nextInt();
+            keyboard.nextLine();
+            if (menuChoice == 1) {
+                System.out.println("Tilføj superhelt: indtast superheltens navn: ");
+                String superHeroName = keyboard.nextLine();
+
+                System.out.println("Indtast superheltens RIGTIGE navn: ");
+                String superHeroRealName = keyboard.nextLine();
+
+                System.out.println("Indtast superheltens superkraft: ");
+                String superHeroSuperPower = keyboard.nextLine();
+
+                System.out.println("Indtast året superhelten blev skabt: ");
+                int superHeroYearCreated = keyboard.nextInt();
+                keyboard.nextLine();
+
+                System.out.println("Er superhelten et menneske? [j/n]");
+                char isHumanInput = keyboard.next().charAt(0);
+                boolean isSuperheroHuman = true;
+                switch (isHumanInput) {
+                    case 'j' -> {
+                    }
+                    case 'n' -> isSuperheroHuman = false;
+                }
+
+                System.out.println("Hvor stærk er superhelten(i tal)?");
+                int superHeroStrength = keyboard.nextInt();
+
+                superheroDatabase.createSuperhero(superHeroName, superHeroRealName, superHeroSuperPower, superHeroYearCreated, isSuperheroHuman, superHeroStrength);
+                superheroDatabase.seeAllHeroes();
+            } else if (menuChoice == 2) {
+                System.out.println("Liste af superhelte: ");
+                superheroDatabase.seeAllHeroes();
+
+            } else if (menuChoice == 3) {
+                System.out.println("indtast søgning: ");
+                String searchSuperhero = keyboard.nextLine();
+                superheroDatabase.findSuperhero(searchSuperhero);
+
+
+            }
+        } while (menuChoice != 9);
+    }
+}
+
